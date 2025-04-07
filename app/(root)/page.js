dd15942afd7dc39caaac4069ea23cbaf84001da8
dhanush <tsdhanush169@gmail.com>
2025-04-08 01:03:00 +0530

    added few things

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/components/hero/Banner";
+import RichTextEditor from "../test/text-editor";
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <RichTextEditor />
     </main>
   )
 }
