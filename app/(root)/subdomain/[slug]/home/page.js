11e51fca4cd7f755232b4f69e2b2a9ec5ba77fcb
dhanush <tsdhanush169@gmail.com>
2025-04-18 23:57:15 +0530

    added generate meta data so that favicon is got from logo

--- app/(root)/subdomain/[slug]/home/page.js
+++ app/(root)/subdomain/[slug]/home/page.js
@@ -1,3 +1,35 @@
+import serverSideFetch from "@/app/service";
+
+// app/[slug]/metadata.js
+export async function generateMetadata({ params }) {
+  const slug = params.slug;
+
+  const data = await serverSideFetch(`/event/${slug}`);
+  console.log(data)
+  const event = data.base_event;
+
+  return {
+    title: event.title,
+    description: event.short_description,
+    openGraph: {
+      title: event.title,
+      description: event.short_description,
+      images: [event.logo],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: event.title,
+      description: event.short_description,
+      images: [event.logo],
+    },
+    icons: {
+      icon: event.logo, // PNG, JPEG, SVG supported
+    },
+  };
+}
+
+
+
 export default async function HackHomePage({params}) {
   const { slug } = (await params)
 
